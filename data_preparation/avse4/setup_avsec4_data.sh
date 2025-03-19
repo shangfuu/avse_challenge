f1305e6f83d0958636e2682dd77dd90bc964cb60
lorena-aldana <34168353+lorena-aldana@users.noreply.github.com>
2025-03-19 16:41:00 +0000

    Update setup_avsec4_data.sh

--- data_preparation/avse4/setup_avsec4_data.sh
+++ data_preparation/avse4/setup_avsec4_data.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # working directory where data will be stored
-root=/disk/data1/aaldana/avsec/ #/disk/data1/aaldana/avsec_LRS3 #/tmp/avsec4/ # EDIT_THIS
+root=/tmp/ # EDIT_THIS
 
 # path to LRS3 data (pretrain and trainval directories should be located there)
-LRS3=/disk/data1/aaldana/LRS3/ #/tmp/LRS3/ # EDIT_THIS
-
-#path to Clarity data
-#clarity_data=/disk/data1/aaldana/ClarityData/clarity_CEC2_data/
+LRS3=/tmp/LRS3/  # EDIT_THIS
+#path to Clarity data and OlHeaD-HRTF Database
 # wget https://data.cstr.ed.ac.uk/cogmhear/protected/clarity_cec2_data.tar
-clarity_data=/tmp/clarity_cec2_data.tar
+clarity_data=/tmp/clarity_cec2_data.tar # EDIT_THIS
 # path to AVSE4 data
 # wget https://data.cstr.ed.ac.uk/cogmhear/protected/avsec4_data.tar
-#avsec4data=/tmp/avse1_data.tar # EDIT_THIS
-avsec4data=/disk/data1/aaldana/avsec4_data.tar
-
+avsec4data=/tmp/avsec4_data.tar # EDIT_THIS
 
 ###########################################################
 # Set up working directory structure and data
@@ -24,31 +20,26 @@ mkdir -p ${root}
 
 tar -xvf ${avsec4data} --directory ${root}/
 
-masker_music=${root}/avsec4/maskers_music/
-masker_noise=${root}/avsec4/maskers_noise/
-masker_speech=${root}/avsec4/maskers_speech/
+masker_music=${root}/maskers_music/
+masker_noise=${root}/maskers_noise/
+masker_speech=${root}/maskers_speech/
 
-mkdir -p ${root}/avsec4/{train,dev}/{targets,interferers,scenes}
+mkdir -p ${root}/{train,dev}/{targets,interferers,scenes}
 
-#Extract HOA_ir and rooms info to dev train folders
-tar -xvf ${clarity_data} --directory ${root}/avsec4/
+#Extract impulse responses and room simulation info to dev/train folders and extract hrir folder
+tar -xvf ${clarity_data} --directory ${root}/
 
-ln -s ${LRS3} ${root}/avsec4/train/targets_video
-ln -s ${LRS3} ${root}/avsec4/dev/targets_video
+ln -s ${LRS3} ${root}/train/targets_video
+ln -s ${LRS3} ${root}/dev/targets_video
 
-ln -s ${masker_music} ${root}/avsec4/train/interferers/music
-ln -s ${masker_music} ${root}/avsec4/dev/interferers/music
-ln -s ${masker_noise} ${root}/avsec4/train/interferers/noise
-ln -s ${masker_noise} ${root}/avsec4/dev/interferers/noise
-
-#sym links clarity data
-ln -s ${clarity_data}/clarity_data/train/rooms ${root}/avsec4/train/rooms
-ln -s ${clarity_data}/clarity_data/dev/rooms ${root}/avsec4/dev/rooms
-ln -s ${clarity_data}/clarity_data/hrir ${root}/avsec4/hrir
+ln -s ${masker_music} ${root}/train/interferers/music
+ln -s ${masker_music} ${root}/dev/interferers/music
+ln -s ${masker_noise} ${root}/train/interferers/noise
+ln -s ${masker_noise} ${root}/dev/interferers/noise
 
 # Create speech masker data from LRS3 videos
-python create_speech_maskers.py ${LRS3} ${root}/avsec4/metadata/masker_speech_list.json ${masker_speech}
+python create_speech_maskers.py ${LRS3} ${root}/metadata/masker_speech_list.json ${masker_speech}
 
-ln -s ${masker_speech} ${root}/avsec4_data/train/interferers/speech
-ln -s ${masker_speech} ${root}/avsec4_data/dev/interferers/speech
+ln -s ${masker_speech} ${root}/train/interferers/speech
+ln -s ${masker_speech} ${root}/dev/interferers/speech
 
